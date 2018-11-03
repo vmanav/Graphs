e4b58c3e1d805e6d1adcc0e966b281df37370050
vmanav <zmanav.1999@gmail.com>
2018-11-03 23:19:14 +0530

    Added Adjacency list implementation STL based

---
+++ vmanav/Code/graph_adj_list_STL.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<list>
+using namespace std;
+
+class graph
+{   private:
+        int v;  //  VERTICES
+        list<int> *adj;
+    public:
+        graph(int V)
+        {   this->v=V;
+            adj = new list<int> [V]; // @vmanav refer geeks for declaration
+        }
+
+        void add_edge(int s , int e)
+        {   adj[s].push_back(e);
+            adj[e].push_back(s);
+        }
+
+        void remove_edge(int s , int e)
+        {   adj[s].remove(e);   
+            adj[s].remove(e);
+        }
+
+        void display()
+        {   int i;
+            for(i=0;i<v;i++)
+            {   list<int>::iterator I= adj[i].begin();
+                cout<<" "<<i<<" - ";
+                while(I!= adj[i].end())
+                {   cout<<" "<<*I;
+                    I++;
+                }
+                cout<<"\n";
+            }  
+        }
+};
+
+int main()
+{   graph g(5);
+
+        g.add_edge(0, 1);
+
+        g.add_edge(0, 4);
+
+        g.add_edge(1, 2);
+
+        g.add_edge(1, 3);
+
+        g.add_edge(1, 4);
+
+        g.add_edge(2, 3);
+
+        g.add_edge(3, 4);
+
+     
+
+        // print the adjacency list representation of the above graph
+
+        g.display();
+    
+    
+   return 0;
+}
