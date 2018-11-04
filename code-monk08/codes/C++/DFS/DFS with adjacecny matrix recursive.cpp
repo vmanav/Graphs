03bed39bb9b78063a5df0778925decd86dc3c8e5
Mayank Singh <mayank.singh081997@gmail.com>
2018-11-04 22:13:56 +0530

    added code for DFS using adjacency list recursive

--- code-monk08/codes/C++/DFS/DFS with adjacecny matrix recursive.cpp
+++ code-monk08/codes/C++/DFS/DFS with adjacecny matrix recursive.cpp
@@ -5,36 +5,26 @@ void DFS(int **edge, int n, int sv, bool *visited)
 {
     cout<<sv<<endl;
     visited[sv] = true;
-
     for(int i=0; i < n; i++)
     {
         if(i == sv)
             continue;
-        if(edge[sv][i] == 1)
-            {
-                if(visited[i])
-                    continue;
-                DFS(edge, n, i, visited);
-            }
+        if(edge[sv][i] == 1 && !visited[i])
+            DFS(edge, n, i, visited);
     }
 }
-
 
 int main()
 {
     int n; //number of vertices
     int e; //number of edges
-
     cin>>n>>e;
-
     int **edge = new int*[n];
     for(int i =0; i < n; i++)
     {
         edge[i] = new int[n];
         for(int j=0; j<n; j++)
-        {
             edge[i][j] = 0;
-        }
     }
     int f; //stores the information of first node in consideration
     int s; //stores the information of second node in consideration
@@ -44,12 +34,9 @@ int main()
         edge[f][s] = 1;
         edge[s][f] = 1;
     }
-
     bool *visited = new bool[n];
     for(int i=0; i < n; i++)
-    {
         visited[i] = false;
-    }
     DFS(edge, n, 0, visited);
     return 0;
 }
